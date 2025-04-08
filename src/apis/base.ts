2411a855581c2216c1b29c3e7ccc8c0f6725a684
gaowei1012 <gyb2020018@163.com>
2025-04-08 14:20:39 +0800

    feat: 新增分页逻辑

--- src/apis/base.ts
+++ src/apis/base.ts
@@ -2,7 +2,7 @@
  * @Author: gaowei1012 gyb2020018@163.com
  * @Date: 2025-03-31 16:35:09
  * @LastEditors: gaowei1012 gyb2020018@163.com
- * @LastEditTime: 2025-04-08 13:55:58
+ * @LastEditTime: 2025-04-08 14:18:18
  * @FilePath: /data-management-terminal/src/apis/base.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -23,16 +23,15 @@ export const postRegister = (data: { userName: string; password: string }) => {
 };
 
 // 错误信息列表
-export const getErrorList = () => {
+export const getErrorList = (page: number, size: number) => {
   return doGetRequest({
-    url: baseURL + `/error/list?pageNum=1&pageSize=10`,
+    url: baseURL + `/error/list?page=${page}&size=${size}`,
   });
 };
 
-
 // 统计信息列表
-export const getMsgList = () => {
+export const getMsgList = (page: number, size: number) => {
   return doGetRequest({
-    url: baseURL + `/msg/list?pageNum=1&pageSize=10`,
+    url: baseURL + `/msg/list?page=${page}&size=${size}`,
   });
 };
